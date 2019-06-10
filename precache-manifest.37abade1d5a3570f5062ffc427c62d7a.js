c9b764b63b6f2bbbf96913628564b836ff8fa01c
Hamed Safi <hd.2033@gmail.com>
2019-06-10 09:25:05 +0430

    Updates

---
+++ precache-manifest.37abade1d5a3570f5062ffc427c62d7a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f668e1c463ea70f9b085b879d11833f",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "ec6cd3f66fbc83015852",
+    "url": "/resume/static/css/main.2b069aa9.chunk.css"
+  },
+  {
+    "revision": "d70f47ec3025fc378f4e",
+    "url": "/resume/static/js/2.1a56b3a5.chunk.js"
+  },
+  {
+    "revision": "ec6cd3f66fbc83015852",
+    "url": "/resume/static/js/main.a1ee9aea.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "1f851630df640828e3d1bb4cfff4c011",
+    "url": "/resume/static/media/css3.1f851630.png"
+  },
+  {
+    "revision": "056741cbdc05953a19c1fac4f8b340e1",
+    "url": "/resume/static/media/jquery.056741cb.png"
+  },
+  {
+    "revision": "f8dab57d048fabd69ea16c67e1615b86",
+    "url": "/resume/static/media/nodejs.f8dab57d.png"
+  },
+  {
+    "revision": "90ca90ad0765bf5b351e3b4542a92849",
+    "url": "/resume/static/media/php.90ca90ad.png"
+  },
+  {
+    "revision": "7dc67391c599ab87b019723ba194c6a3",
+    "url": "/resume/static/media/python.7dc67391.png"
+  },
+  {
+    "revision": "2260eb1a0f48eb82d60fd7493339a9f7",
+    "url": "/resume/static/media/reactjs.2260eb1a.png"
+  }
+]);
